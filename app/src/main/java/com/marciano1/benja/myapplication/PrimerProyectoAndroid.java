0c82b881021dbcb52f982e903a9d112514ba7347
Benjamin Galicia <benja9854@hotmail.com>
2017-07-10 14:24:12 -0500

    Initial commit

---
+++ app/src/main/java/com/marciano1/benja/myapplication/PrimerProyectoAndroid.java
@@ -0,0 +1,13 @@
+package com.marciano1.benja.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimerProyectoAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primer_proyecto_android);
+    }
+}
